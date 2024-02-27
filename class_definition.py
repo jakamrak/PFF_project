a1e9fa9746de647f238ad8eb8cbb87003e7f6fe6
Jaka mrak <mrak.jaka9@gmail.com>
2024-02-27 01:36:14 +0000

    added data import, cleaning and correlation

---
+++ class_definition.py
@@ -0,0 +1,19 @@
+## Lets create a class to manage the data of stocks
+#class Stocks:
+#  def __init__(self):
+#    self.dic = {}
+#    
+#  def add(self, ticker, data):
+#    self.dic[ticker] = data
+#
+#  def get_data(self, ticker):
+#    return self.dic[ticker]
+#
+#  def __len__(self):
+#    return len(self.dic)
+#
+#  @classmethod
+#  def help(cls):
+#    print("This class manages stock data")
+#
+## Here we will have another class on which we will define all atributes and methods used in trader
